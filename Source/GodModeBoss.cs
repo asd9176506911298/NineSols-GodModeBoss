ba43759c6a54e316c70c7f6219818a9092af4702
Yuki.kaco <a0936660023@gmail.com>
2024-11-12 18:02:08 +0800

    Init

---
+++ Source/GodModeBoss.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using NineSolsAPI;
+using UnityEngine;
+
+namespace GodModeBoss;
+
+[BepInDependency(NineSolsAPICore.PluginGUID)]
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class GodModeBoss : BaseUnityPlugin {
+    // https://docs.bepinex.dev/articles/dev_guide/plugin_tutorial/4_configuration.html
+    private ConfigEntry<bool> enableSomethingConfig = null!;
+    private ConfigEntry<KeyboardShortcut> somethingKeyboardShortcut = null!;
+
+    private Harmony harmony = null!;
+
+    private void Awake() {
+        Log.Init(Logger);
+        RCGLifeCycle.DontDestroyForever(gameObject);
+
+        harmony = Harmony.CreateAndPatchAll(typeof(GodModeBoss).Assembly);
+
+        KeybindManager.Add(this, TestMethod, () => somethingKeyboardShortcut.Value);
+
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+
+    private void TestMethod() {
+        if (!enableSomethingConfig.Value) return;
+        ToastManager.Toast("Shortcut activated");
+
+    }
+
+    private void OnDestroy() {
+
+        harmony.UnpatchSelf();
+    }
+}
--- Source/Log.cs
+++ Source/Log.cs
@@ -1,6 +1,6 @@
 using BepInEx.Logging;
 
-namespace ExampleMod;
+namespace GodModeBoss;
 
 internal static class Log {
     private static ManualLogSource? logSource;
